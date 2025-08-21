0050dd63d299f03547d8419732e5f05a70c08bae
Santi <sweihmuller01@gmail.com>
2025-08-21 16:47:09 -0300

    Validate form submit and showing error message

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,8 +1,12 @@
-﻿namespace ManejoPresupuesto.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
 {
     public class TipoCuenta
     {
-        public int id  { get; set; }
+        public int id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [StringLength(maximumLength: 50, MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
