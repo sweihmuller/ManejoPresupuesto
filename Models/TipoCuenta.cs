7ddcd2e6ce28189214a368b21dab5a5f0174f129
Santi <sweihmuller01@gmail.com>
2025-08-22 13:20:33 -0300

    Set validation in the Front End

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -5,8 +5,9 @@ namespace ManejoPresupuesto.Models
     public class TipoCuenta
     {
         public int id { get; set; }
+        [Display(Name = "Nombre del tipo de cuenta")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
         [Required(ErrorMessage = "El campo {0} es requerido.")]
-        [StringLength(maximumLength: 50, MinimumLength =3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
