4bcd28a77efe8756df419adc1ee0026a8de41f3a
Santi <sweihmuller01@gmail.com>
2025-08-23 16:20:01 -0300

    Making custom validations by attributes

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
 
 namespace ManejoPresupuesto.Models
 {
@@ -6,8 +7,7 @@ namespace ManejoPresupuesto.Models
     {
         public int id { get; set; }
         [Display(Name = "Nombre del tipo de cuenta")]
-        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
-        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [PrimeraLetraMaysucula]
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
